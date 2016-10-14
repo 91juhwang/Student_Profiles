87314f4db8bb10b8c266fab1228a9234f56b70b6
91juhwang <91juhwang@gmail.com>
2016-10-14 14:58:53 -0400

    navigations, skills, routing fixed and delete method added for skills section

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,6 +6,13 @@ class SkillsController < ApplicationController
 		redirect_to profile_path(@profile.id)
 	end
 
+	def destroy
+		@profile = Profile.find(params[:profile_id])
+		@skill = @profile.skills.find(params[:id])
+		@skill.destroy
+		redirect_to profile_path(@profile.id)
+	end
+
 	private
 	def skill_params
 		params.require(:skill).permit(:profile_id, :name)
