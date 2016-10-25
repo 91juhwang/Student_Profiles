2b09ce5066ee6fae92d2038e3fedb6997cc39122
Benjamin Kim <benjaminkim14@gmail.com>
2016-10-25 17:20:39 -0400

    job crud added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012190346) do
+ActiveRecord::Schema.define(version: 20161025201809) do
 
   create_table "educations", force: :cascade do |t|
     t.string   "school"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20161012190346) do
     t.text     "description"
     t.datetime "start"
     t.datetime "end"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "company"
+    t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
